c7b98c33fed70f09f12d5f360bc243ccc23716d0
ethxndxniels <ethan2015d@gmail.com>
2022-09-11 12:42:44 -0500

    Cube color working, geometry still wonky.

--- KatameVR/src/Geometry/Geometry.h
+++ KatameVR/src/Geometry/Geometry.h
@@ -4,7 +4,8 @@
 
 namespace Geometry {
 
-    struct CubeVertex {
+    struct CubeVertex 
+	{
         XrVector3f Position;
         XrVector3f Color;
     };
@@ -16,18 +17,28 @@ namespace Geometry {
     constexpr XrVector3f Blue{ 0, 0, 1 };
     constexpr XrVector3f DarkBlue{ 0, 0, 0.25f };
 
+	// Vertices for a 1x1x1 meter cube. (Left/Right, Top/Bottom, Front/Back)
+	constexpr XrVector3f LBB{ -0.5f, -0.5f, -0.5f };
+	constexpr XrVector3f LBF{ -0.5f, -0.5f, 0.5f };
+	constexpr XrVector3f LTB{ -0.5f, 0.5f, -0.5f };
+	constexpr XrVector3f LTF{ -0.5f, 0.5f, 0.5f };
+	constexpr XrVector3f RBB{ 0.5f, -0.5f, -0.5f };
+	constexpr XrVector3f RBF{ 0.5f, -0.5f, 0.5f };
+	constexpr XrVector3f RTB{ 0.5f, 0.5f, -0.5f };
+	constexpr XrVector3f RTF{ 0.5f, 0.5f, 0.5f };
+
+#define CUBE_SIDE(V1, V2, V3, V4, V5, V6, COLOR) {V1, COLOR}, {V2, COLOR}, {V3, COLOR}, {V4, COLOR}, {V5, COLOR}, {V6, COLOR},
+
 	static std::vector<CubeVertex> CreateCubeVertices()
 	{
 		return std::vector<CubeVertex>
 		{
-			{ { -3.0f, 3.0f, -3.0f }, { 0, 0, 255 } },
-			{ { 3.0f, 3.0f, -3.0f}, { 0, 255, 0 } },
-			{ { -3.0f, -3.0f, -3.0f }, { 255, 0, 0 } },
-			{ { 3.0f, -3.0f, -3.0f }, { 0, 255, 255 } },
-			{ { -3.0f, 3.0f, 3.0f }, { 0, 0, 255 } },
-			{ { 3.0f, 3.0f, 3.0f }, { 255, 0, 0 } },
-			{ { -3.0f, -3.0f, 3.0f }, { 0, 255, 0 } },
-			{ { 3.0f, -3.0f, 3.0f }, { 0, 255, 255 } },
+			CUBE_SIDE( LTB, LBF, LBB, LTB, LTF, LBF, DarkRed )    // -X
+			CUBE_SIDE( RTB, RBB, RBF, RTB, RBF, RTF, Red )        // +X
+			CUBE_SIDE( LBB, LBF, RBF, LBB, RBF, RBB, DarkGreen )  // -Y
+			CUBE_SIDE( LTB, RTB, RTF, LTB, RTF, LTF, Green )      // +Y
+			CUBE_SIDE( LBB, RBB, RTB, LBB, RTB, LTB, DarkBlue )   // -Z
+			CUBE_SIDE( LBF, LTF, RTF, LBF, RTF, RBF, Blue )       // +Z
 		};
 	}
 
@@ -35,18 +46,12 @@ namespace Geometry {
 	{
 		return std::vector<unsigned int>
 		{
-			0, 1, 2,
-				2, 1, 3,
-				4, 0, 6,
-				6, 0, 2,
-				7, 5, 6,
-				6, 5, 4,
-				3, 1, 7,
-				7, 1, 5,
-				4, 5, 0,
-				0, 5, 1,
-				3, 7, 2,
-				2, 7, 6,
+			0, 1, 2, 3, 4, 5,   // -X
+			6, 7, 8, 9, 10, 11,  // +X
+			12, 13, 14, 15, 16, 17,  // -Y
+			18, 19, 20, 21, 22, 23,  // +Y
+			24, 25, 26, 27, 28, 29,  // -Z
+			30, 31, 32, 33, 34, 35,  // +Z
 		};;
 	}
 
